77601adf31fdb3f117a408a04c1f4d0c8bb488ab
Matthew Kokotovich <mjkokotovich@spscommerce.com>
2018-07-06 12:12:55 -0500

    Adds configs for logging in to spsalpha.auth0.com

---
+++ 03-Calling-an-API/src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'spsalpha.auth0.com',
+  clientId: '7Z6q5mPyuLhP7esBb69BWVu5IvnbH3gJ',
+  callbackUrl: 'http://localhost:3000/callback',
+  apiUrl: 'https://api.spscommerce.com'
+}
